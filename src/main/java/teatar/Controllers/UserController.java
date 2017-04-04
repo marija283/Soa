dbfc096d4e74cfb02997d683da21e225811ec184
Marija Todorova <marija283@hotmail.com>
2017-04-04 16:21:08 +0200

    hello

--- src/main/java/teatar/Controllers/UserController.java
+++ src/main/java/teatar/Controllers/UserController.java
@@ -1,11 +1,14 @@
 package teatar.Controllers;
 
+import hello.Greeting;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import teatar.Entities.Users;
 import teatar.Repository.UserRepo;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by Marija on 3/14/2017.
@@ -15,6 +18,15 @@ public class UserController {
 
     @Autowired
     UserRepo userRepo;
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
 
     @RequestMapping("/createNewUser")
     public Users CreateNew(@RequestParam(value = "name") String name, @RequestParam String surname
